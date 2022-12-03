402fc60fb6f4583ca8ecf544108514c644de7165
YukishaMA <18030671@itcelaya.edu.mx>
2022-12-03 17:54:22 -0600

    Base de datos conectada, min 41

---
+++ src/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/equidad-genero-db',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+)   .then(db => console.log('DB is connected'))
+    .catch(err => console.error(err));
+
+
